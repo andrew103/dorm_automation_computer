6f07ae829c0bcfc857a9013aebc3ce059ca08d94
Andrew Euredjian <andrew_103@rocketmail.com>
2018-10-15 15:33:31 -0700

    Rewrite hotword detection to use speech recognition library snowboy implementation

--- user_interface/main.py
+++ user_interface/main.py
@@ -6,41 +6,33 @@ from pygame import mixer
 import requests
 
 r = sr.Recognizer()
-# m = sr.Microphone()
+m = sr.Microphone()
 
 abs_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-def receive_command():
-    detector.terminate()
-    global interrupted
-    interrupted = True
+def receive_command(audio_in):
+    # print('Computer Activated')
 
-    print('Computer Activated')
+    # mixer.init(44100)
+    # mixer.music.load(os.path.join(abs_path, 'audio/computerbeep_42.mp3'))
+    # mixer.music.play()
 
-    mixer.init(44100)
-    mixer.music.load(os.path.join(abs_path, 'audio/computerbeep_42.mp3'))
-    mixer.music.play()
+    try:
+        value = r.recognize_google(audio_in)
+        uri = value.replace(" ", "_")
+        response = requests.get('http://127.0.0.1:8000/{}'.format(uri))
 
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        print("Listening...")
-        audio = r.listen(source, timeout=5)
-        try:
-            value = r.recognize_google(audio)
-            uri = value.replace(" ", "_")
-            response = requests.get('http://127.0.0.1:8000/{}'.format(uri))
+        if response.status_code != 200:
+            print("Command does not exist")
+        else:
+            print(response.text)
 
-            if response.status_code != 200:
-                print("Command does not exist")
-            else:
-                print(response.text)
-
-        except sr.UnknownValueError:
-            print("Uknown value")
-        except sr.RequestError:
-            print("Couldn't reach Google Speech Recognition service")
-        except requests.exceptions.ConnectionError:
-            print("Backend server could not be reached")
+    except sr.UnknownValueError:
+        print("Uknown value")
+    except sr.RequestError:
+        print("Couldn't reach Google Speech Recognition service")
+    except requests.exceptions.ConnectionError:
+        print("Backend server could not be reached")
 
     # print('Computer Activated')
     #
@@ -71,39 +63,16 @@ def receive_command():
 
 #=================================== BEGIN SNOWBOY DETECTOR CONFIG AND LISTENER
 
-interrupted = False
-
-
-def signal_handler(signal, frame):
-    sys.exit(0)
-    # global interrupted
-    # interrupted = True
-
-
-def interrupt_callback():
-    global interrupted
-    return interrupted
-
 # models = os.listdir('./models')
 models = ['models/computer1.pmdl', 'models/computer2.pmdl']
-
-# capture SIGINT signal, e.g., Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
-
-sensitivity = [0.45]
 callbacks = [lambda: receive_command()]*len(models)
-
-print('Listening... Press Ctrl+C to exit')
 
 # main loop
 # make sure you have the same numbers of callbacks and models
 # THIS IS A TEMPORARY SOLUTION THAT WILL BE REDONE WITH MULTITHREADING
 while True:
-    detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
-
-    interrupted = False
-    detector.start(detected_callback=callbacks,
-                   interrupt_check=interrupt_callback,
-                   sleep_time=0.03)
-
-detector.terminate()
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+        print("Listening...")
+        audio = r.listen(source, snowboy_configuration=('snowboy_ubuntu', models))
+        receive_command(audio)

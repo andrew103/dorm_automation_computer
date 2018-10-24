82f4a23f31d33ff7cdf28894f08f9e3c3e532332
Andrew Euredjian <andrew_103@rocketmail.com>
2018-10-24 17:33:24 -0400

    Make built in snowboy for speech recognition library work

--- user_interface/main.py
+++ user_interface/main.py
@@ -10,15 +10,17 @@ m = sr.Microphone()
 
 abs_path = os.path.dirname(os.path.abspath(sys.argv[0]))
 
-def receive_command(audio_in):
-    # print('Computer Activated')
+def receive_command():
+    print('Computer Activated')
 
-    # mixer.init(44100)
-    # mixer.music.load(os.path.join(abs_path, 'audio/computerbeep_42.mp3'))
-    # mixer.music.play()
+    mixer.init(44100)
+    mixer.music.load(os.path.join(abs_path, 'audio/computerbeep_42.mp3'))
+    mixer.music.play()
 
     try:
-        value = r.recognize_google(audio_in)
+        audio = r.listen(source, timeout=10)
+        value = r.recognize_google(audio)
+        # print(value)
         uri = value.replace(" ", "_")
         response = requests.get('http://127.0.0.1:8000/{}'.format(uri))
 
@@ -33,32 +35,8 @@ def receive_command(audio_in):
         print("Couldn't reach Google Speech Recognition service")
     except requests.exceptions.ConnectionError:
         print("Backend server could not be reached")
-
-    # print('Computer Activated')
-    #
-    # mixer.init(44100)
-    # mixer.music.load(os.path.join(abs_path, 'audio/computerbeep_42.mp3'))
-    # mixer.music.play()
-    #
-    # with m as source: r.adjust_for_ambient_noise(source)
-    # print("Listening...")
-    # with m as source: audio = r.listen(source)
-    # try:
-    #     value = r.recognize_google(audio)
-    #     uri = value.replace(" ", "_")
-    #     response = requests.get('http://127.0.0.1:8000/{}'.format(uri))
-    #
-    #     if response.status_code != 200:
-    #         print("Command does not exist")
-    #     else:
-    #         print(response.text)
-    #
-    # except sr.UnknownValueError:
-    #     print("Uknown value")
-    # except sr.RequestError:
-    #     print("Couldn't reach Google Speech Recognition service")
-    # except requests.exceptions.ConnectionError:
-    #     print("Backend server could not be reached")
+    except:
+        print("Recognizer timed out")
 
 
 #=================================== BEGIN SNOWBOY DETECTOR CONFIG AND LISTENER
@@ -68,11 +46,12 @@ models = ['models/computer1.pmdl', 'models/computer2.pmdl']
 callbacks = [lambda: receive_command()]*len(models)
 
 # main loop
-# make sure you have the same numbers of callbacks and models
-# THIS IS A TEMPORARY SOLUTION THAT WILL BE REDONE WITH MULTITHREADING
+with m as source: r.adjust_for_ambient_noise(source)
 while True:
     with m as source:
-        r.adjust_for_ambient_noise(source)
         print("Listening...")
-        audio = r.listen(source, snowboy_configuration=('snowboy_ubuntu', models))
-        receive_command(audio)
+        try:
+            r.snowboy_wait_for_hot_word('snowboy_ubuntu', models, source)
+            receive_command()
+        except KeyboardInterrupt:
+            break
